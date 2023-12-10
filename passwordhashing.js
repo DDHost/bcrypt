8c606364f7124fb8cedeeb3ee6157b5b491377ce
DDHost <41550297+DDHost@users.noreply.github.com>
2023-12-10 21:36:14 +0200

    Update and rename test.js to passwordhashing.js

--- passwordhashing.js
+++ passwordhashing.js
@@ -4,5 +4,8 @@ const hashPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 };
-const logPass = async (pass) => console.log(await hashPassword(pass));
-logPass('test');
+// For testing
+//const logPass = async (pass) => console.log(await hashPassword(pass));
+//logPass('test');
+
+module.exports = hashPassword;
